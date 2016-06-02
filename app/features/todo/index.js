e23c3f66d36684693e1d3fa347548dd668299278
kusi0 <mi.kusmierczyk@gmail.com>
2016-06-02 20:49:55 +0000

    Add directive task-list, and connection to firebase

--- app/features/todo/index.js
+++ app/features/todo/index.js
@@ -1,14 +1,21 @@
 import angular from 'angular';
 import uirouter from 'angular-ui-router';
 
+
 import routing from './todo.routes';
 
 import ToDoController from './todo.controller';
 
 import taskInput from './directives/task-input';
+import taskList from './directives/task-list';
 
-export default angular.module('app.todo', [uirouter])
+import angularFire from 'angularfire';
+import firebase from 'firebase';
+
+
+export default angular.module('app.todo', [uirouter,  angularFire])
   .config(routing)
   .controller('ToDoController',ToDoController)
   .directive('taskInput', taskInput)
+  .directive('taskList', taskList)
   .name;
--- app/features/todo/todo.controller.js
+++ app/features/todo/todo.controller.js
@@ -1,11 +1,28 @@
+/*global Firebase */
 export default class ToDoController {
   
-  constructor(LoginService) { 
+  constructor(LoginService, $firebaseArray) { 
     this.LoginService = LoginService;
-    this.listOfTask = [];
+    var ref  = new Firebase("https://angular-project-1826a.firebaseio.com/");
+    this.items = $firebaseArray(ref);
   }
   
   addTask(){
-    this.listOfTask.push(this.taskName);
+    
+   // this.task.name = this.taskName;
+   // this.task.status = "active";
+   // this.task.owner = this.LoginService.name;
+    
+  //  this.listOfTask.push(this.task);
+  
+  
+  this.items.$add({
+      desc: this.taskName, 
+      owner: this.LoginService.name,
+      status: "active"
+      
+  });
+  console.log(this.listOfTask);
+ //   console.log(myData);
   }
 }
